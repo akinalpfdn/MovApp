1cc7c5ac39d70dd7c806751f2eccbce06584ac99
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2025-10-06 20:56:54 +0300

    Initial Commit

---
+++ MovApp/MovAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MovAppApp.swift
+//  MovApp
+//
+//  Created by Akinalp Fidan on 6.10.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MovAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
